1de2058dc627bbf0ea72f0c17320e910d6a404f7
Claire Illich <claire@gingerlabs.com>
2019-11-02 14:28:38 -0700

    First commit for iOS app

---
+++ BravenInterviewApp/BravenInterviewApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BravenInterviewApp
+//
+//  Created by Claire Illich on 11/2/19.
+//  Copyright © 2019 Team 19. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
